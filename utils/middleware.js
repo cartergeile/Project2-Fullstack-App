42168a8c3910bb40cb5db7ba61a209cc82e16351
Carter Geile <cartergeile@l.local>
2023-01-25 14:59:11 -0600

    users can now log in

--- utils/middleware.js
+++ utils/middleware.js
@@ -3,6 +3,9 @@
 /*----------------------*/
 const express = require('express')
 const morgan = require('morgan')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')
+require('dotenv').config()
 
 /*-----------------------*/
 // MIDDLEWARE FUNCTION  //
@@ -12,6 +15,16 @@ const middleware = (app) => {
   app.use(express.urlencoded({ extended: true}))
   app.use(express.static('public'))
   app.use(express.json())
+  app.use(
+    session({
+      secret: process.env.SECRET,
+      store: MongoStore.create({
+        mongoUrl: process.env.DATABASE_URL
+      }),
+      saveUninitialized: true,
+      resave: false
+    })
+  )
 }
 
 /*-----------------------*/
